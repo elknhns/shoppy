afee71b8a999392210b9adca1b65306145195d9c
elkana.widersen <elkana.widersen@seamoney.com>
2023-07-18 11:01:22 +0700

    feat: change home page billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 export default async function HomePage() {
 	const { data: products } = await getProducts({ isFeatured: true });
 	const { data: billboard } = await getBillboard(
-		'0ff587f4-622e-4214-9329-c9f8df13076c'
+		'efb59b84-ade4-4110-b13d-a284453d06b8'
 	);
 
 	return (
